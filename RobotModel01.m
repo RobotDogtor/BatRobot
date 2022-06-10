b49aaa163f37fb636a09f77e7a2ffe27717fc907
Ben Beiter <bbeiter1@vt.edu>
2022-06-10 19:12:05 +0800

    full bat and wing plot

--- RobotModel01.m
+++ RobotModel01.m
@@ -57,17 +57,23 @@ sp0 = [0; 0; 0];
 sp1 = [-b2; -b1; -b0];
 sp2 = [-b9; -(s1/2)-s8; -b10];
 sp3 = [-b11; -(s1/2); -b12];
+sp4 = [-b13; -b14; -b15];
+
+sp = [sp1,sp2,sp3,sp4];
 
 %% Process Variable
-theta = 0;
+theta = 3*pi/2;
 
 %% body Points
 bp7 = sp3 + [b6*sin(theta); -s8; b6*cos(theta)];
-bp8 = bp7 + [0; s9; 0];
+bp8 = bp7 + [0; -s9; 0];
 
-%bp5 is a certain distance from sp2 and bp5
+%bp9 is a certain distance from sp2 and bp5
 [p3x,p3y] = ThirdPointSolver_Upper(sp2(1),sp2(3),bp7(1),bp7(3),b3,b12);
-bp5 = [p3x; sp2(2); p3y];
+bp9 = [p3x; sp2(2); p3y];
+%bp6 is in the direction sp2 to bp9
+dir = (bp9 - sp2)/b3;
+bp6 = sp2 + dir*(b3+b4);
 
 %bp2 is a certain distance from sp1 and bp8 in 3D, x is fixed however
 %center 1 is the center of the first circle based around sp1
@@ -79,6 +85,7 @@ center2_x_diff = center1_x - bp8(1);
 b7_altered = sqrt(b7^2 - center2_x_diff^2);
 [p3x,p3y] = ThirdPointSolver_Outer(sp1(2),sp1(3),bp7(2),bp7(3),c2,b7_altered);
 bp2 = [center1_x; p3x; p3y];
+cp0 = bp2 + [-c3; 0; 0];
 
 %beta (wing angle) can now be defined as the angle that bp2 makes with sp1
 hypot = bp2(2:3) - sp1(2:3);
@@ -90,9 +97,71 @@ bp1 = sp1 + [0; c1*hypot_hat];
 
 %bp3 is sp1 plus the variable v1 in x
 %yz position of bp5 is 
-bp5yz = 
+bp5yz = sp1(2:3) + [-c5*sin(beta); -c5*cos(beta)];
+%bp5 is a certain distance from bp6. Distance in x can be found
+bp5_to_bp6_xDist = sqrt( b5^2 - (bp5yz(1)-bp6(2))^2 - (bp5yz(2)-bp6(3))^2 );
+bp5 = [bp6(1)-bp5_to_bp6_xDist; bp5yz];
+bp3 = [bp5(1); sp1(2:3)];
+bp4 = [bp5(1); sp1(2)-c4*cos(beta); sp1(3)+c4*sin(beta)]; 
+v1 = abs(bp3(1)-sp1(1));
+
+bp = [bp1,bp2,bp3,bp4,bp5,bp6,bp7,bp8];
+
+%% 2D wing points (in x and y as if beta = 0)
+wp1 = [0; 0; 0];
+wp2 = [-v1; 0; 0];
+[p3x,p3y] = ThirdPointSolver_Outer(wp1(1),wp1(2),wp2(1),wp2(2),w1,w2);
+wp3 = [p3x; p3y; 0];
+dir = (wp3-wp1)/w1;
+wp5 = wp3 + dir*w4;
+wp6 = wp5 + dir*w6;
+[p3x,p3y] = ThirdPointSolver_Outer(wp5(1),wp5(2),wp2(1),wp2(2),w5,w3);
+wp4 = [p3x; p3y; 0];
+dir = (wp5-wp4)/w5;
+wp7 = wp5 + dir*w7;
+[p3x,p3y] = ThirdPointSolver_Lower(wp7(1),wp7(2),wp6(1),wp6(2),w9,w8);
+wp8 = [p3x; p3y; 0];
+dir = (wp8-wp7)/w9;
+wp9 = wp8 + dir*w10;
+
+wp2d = [wp1,wp2,wp3,wp4,wp5,wp6,wp7,wp8,wp9];
+
+rotateWingPoints = @(wPts,angle,basePoint) [wPts(1,:); wPts(2,:)*cos(angle); -wPts(2,:)*sin(angle)] + basePoint;
+wp = rotateWingPoints(wp2d,beta,bp1);
 
 %% collect and plot all points
 
+plotPoints = @(setOfPoints,color) plot3(setOfPoints(1,:),setOfPoints(2,:),setOfPoints(3,:),color);
+plotPointsFlip = @(setOfPoints,color) plot3(setOfPoints(1,:),-setOfPoints(2,:),setOfPoints(3,:),color);
 
+linesK1 = [sp3 bp7 bp8 bp2];
+linesK2 = [bp7 bp9 sp2 bp6 bp5];
+linesB1 = [bp5 bp3 bp4];
+linesB2 = [bp2 cp0 bp1 sp1];
+linesR12d = [wp3 wp2 wp4 wp7 wp9 wp8 wp6 wp1];
+linesR1 = rotateWingPoints(linesR12d,beta,bp1);
 
+figure(1)
+hold on
+grid on
+axis equal
+plotPoints(sp,'ko')
+plotPoints(bp,'bo')
+plotPoints(wp,'rx')
+plotPoints(linesK1,'k')
+plotPoints(linesK2,'k')
+plotPoints(linesB1,'b')
+plotPoints(linesB2,'b')
+plotPoints(linesR1,'r')
+plotcube([s10 s1 15],[-s10 -s1/2 -15],0.8,[0 1 0.2])
+
+plotPointsFlip(sp,'ko')
+plotPointsFlip(bp,'bo')
+plotPointsFlip(wp,'rx')
+plotPointsFlip(linesK1,'k')
+plotPointsFlip(linesK2,'k')
+plotPointsFlip(linesB1,'b')
+plotPointsFlip(linesB2,'b')
+plotPointsFlip(linesR1,'r')
+plotcube([s1*1.5 s1 s1/1.5],[-s1*1.5 -s1/2 -15-s1/1.5],.8,[0 1 0.2]);
+    
---
+++ ThirdPointSolver_Lower.m
@@ -0,0 +1,14 @@
+function [p3x,p3y] = ThirdPointSolver_Lower(p1x,p1y,p2x,p2y,d13,d23)
+%Returns only the upper solution (in y) of the Third Point Solver
+
+[p3x_1,p3y_1,p3x_2,p3y_2] = ThirdPointSolver(p1x,p1y,p2x,p2y,d13,d23);
+
+if (p3y_1<p3y_2)
+    p3x = p3x_1;
+    p3y = p3y_1;
+else
+    p3x = p3x_2;
+    p3y = p3y_2;
+end
+end
+
